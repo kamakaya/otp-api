e8c5f1ea3cd2bce90b7c19a551bcd8894b8793e9
Rommel Silva <kamakayasilva@gmail.com>
2023-03-22 11:09:16 -0600

    updated the sync urls for dev and prod with the cloud run endpoint

--- helpers/gcp_tools/__init__.py
+++ helpers/gcp_tools/__init__.py
@@ -4,15 +4,14 @@ from pathlib import Path
 #TODO: Update all config info on here
 
 service_account_path = Path('one-time-password-via-api-b715a9a5e72b.json').resolve()
-GCP_PROJECT_ID = '966262794836'
+GCP_PROJECT_ID = '966262794836' 
 GCLOUD_PROJECT = 'one-time-password-via-api'
 GCLOUD_REGION = 'us-west1'
 GCLOUD_CREDENTIALS = service_account.Credentials.from_service_account_file(str(service_account_path))
 GCLOUD_SA_EMAIL = 'otp-sa@one-time-password-via-api.iam.gserviceaccount.com'
-#APP_URL = 'thinkific-email-migration-btpeav32vq-uc.a.run.app'
+APP_URL = 'https://one-time-password-via-api-rzmipdxtnq-uw.a.run.app'
 
-# TODO: CHANGE THIS TO PRODUCTION URL BEFORE GO-LIVE
-# SYNC_URLS = {
-#     'DEV': f'https://development---{APP_URL}/process_user',
-#     'PROD': f'https://production---{APP_URL}/process_user'
-# }
+SYNC_URLS = {
+    'DEV': f'https://development---{APP_URL}/get_otp',
+    'PROD': f'https://production---{APP_URL}/get_otp'
+}
